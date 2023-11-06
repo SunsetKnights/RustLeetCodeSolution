608c23500fa707ce49846141c84118eaf7d8e37c
SunsetKnights <sunsetknightss@gmail.com>
2023-11-06 14:59:46 +0800

    318. 最大单词长度乘积

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-pub mod three_sum_closest;
+pub mod max_product;
 
 fn main() {
-    let nums = vec![-1, 2, 1, -4];
-    let target = 1;
-    let result = three_sum_closest::Solution::three_sum_closest(nums, target);
+    let nums: Vec<String> = vec![
+        "abcw".to_string(),
+        "baz".to_string(),
+        "foo".to_string(),
+        "bar".to_string(),
+        "xtfn".to_string(),
+        "abcdef".to_string(),
+    ];
+    let result = max_product::Solution::max_product(nums);
     println!("{}", result);
 }
