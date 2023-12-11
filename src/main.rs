cba217f4444e85aeba13d01097504d4b83a562c1
SunsetKnights <sunsetknightss@gmail.com>
2023-12-11 14:15:23 +0800

    1631. 最小体力消耗路径

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-pub mod max_taxi_earnings;
-use max_taxi_earnings::*;
+pub mod minimum_effort_path;
+use minimum_effort_path::*;
 
 fn main() {
-    let n = 5;
-    let rides = vec![vec![2, 5, 4], vec![1, 5, 1]];
-    let result = Solution::max_taxi_earnings(n, rides);
+    let heights = vec![vec![1, 2, 3], vec![3, 8, 4], vec![5, 3, 5]];
+    let result = Solution::minimum_effort_path(heights);
     println!("{}", result);
 }
