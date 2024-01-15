15c3fb0380d0b0b641499a74b77125b3984d5dd4
SunsetKnights <sunsetknightss@gmail.com>
2024-01-15 13:40:59 +0800

    [200~82. 删除排序链表中的重复元素 II

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-pub mod trie;
-use trie::*;
+pub mod delete_duplicates;
+use delete_duplicates::*;
 
 fn main() {
-    let mut t = Trie::new();
-    t.insert("apple".to_string());
-    t.search("apple".to_string());
-    t.search("app".to_string());
-    t.starts_with("app".to_string());
-    t.insert("app".to_string());
-    t.search("app".to_string());
+    let link_list_val = [1, 2, 3, 3, 4, 4, 5];
+    let mut head = None;
+    for &val in link_list_val.iter().rev() {
+        let mut new_node = ListNode::new(val);
+        new_node.next = head;
+        head = Some(Box::new(new_node));
+    }
+    let mut result = Solution::delete_duplicates(head);
+    let mut print_string = String::new();
+    while let Some(node) = result {
+        print_string.push_str(node.val.to_string().as_str());
+        print_string.push_str("->");
+        result = node.next;
+    }
+    print_string.push_str("None");
+    println!("{print_string}");
 }
