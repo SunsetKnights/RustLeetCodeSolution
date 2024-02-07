2c218c64d739711dbd1e62f8ff3a50e52307323a
SunsetKnights <sunsetknightss@gmail.com>
2024-02-07 20:56:23 +0800

    2641. 二叉树的堂兄弟节点 II

--- src/inner_mutable_tree.rs
+++ src/inner_mutable_tree.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -9,12 +10,54 @@ pub struct TreeNode {
 }
 impl TreeNode {
     #[inline]
-    pub fn new(val: i32) -> Self {
+    fn new(val: i32) -> Self {
         TreeNode {
             val,
             left: None,
             right: None,
         }
+    }
+
+    pub fn create_from_vec(data: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        // i32::MAX means null
+        if data.is_empty() || data[0] == i32::MAX {
+            return None;
+        }
+        let mut queue = VecDeque::new();
+        let root = Rc::new(RefCell::new(Self::new(data[0])));
+        queue.push_back(Rc::clone(&root));
+        for idx in 1..data.len() {
+            let curr_node = queue.pop_front().unwrap();
+            if data[idx] != i32::MAX {
+                let child = Rc::new(RefCell::new(TreeNode::new(data[idx])));
+                queue.push_back(Rc::clone(&child));
+                if idx & 1 == 1 {
+                    curr_node.borrow_mut().left = Some(child);
+                } else {
+                    curr_node.borrow_mut().right = Some(child);
+                }
+            }
+        }
+        Some(root)
+    }
+
+    pub fn to_vec(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut ret = Vec::new();
+        if let Some(root) = root {
+            let mut queue = VecDeque::new();
+            ret.push(root.borrow().val);
+            queue.push_back(root);
+            while let Some(node) = queue.pop_front() {
+                ret.push(node.borrow().val);
+                if node.borrow().left.is_some() {
+                    queue.push_back(node.borrow_mut().left.take().unwrap());
+                }
+                if node.borrow().right.is_some() {
+                    queue.push_back(node.borrow_mut().right.take().unwrap());
+                }
+            }
+        }
+        ret
     }
 }
 
@@ -94,5 +137,61 @@ impl Solution {
             }
         }
         ret
+    }
+
+    /**
+     * 2641. 二叉树的堂兄弟节点 II
+     * 给你一棵二叉树的根 root ，请你将每个节点的值替换成该节点的所有 堂兄弟节点值的和 。
+     * 如果两个节点在树中有相同的深度且它们的父节点不同，那么它们互为 堂兄弟 。
+     * 请你返回修改值之后，树的根 root 。
+     * 注意，一个节点的深度指的是从树根节点到这个节点经过的边数。
+     */
+    pub fn replace_value_in_tree(
+        root: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut queue = VecDeque::new();
+        queue.push_back((
+            Rc::clone(root.as_ref().unwrap()),
+            root.as_ref().unwrap().borrow().val,
+        ));
+        while !queue.is_empty() {
+            let level_sum: i32 = queue.iter().map(|(node, _)| node.borrow().val).sum();
+            let mut new_queue = VecDeque::new();
+            for (node, bro_sum) in queue {
+                node.borrow_mut().val = level_sum - bro_sum;
+                let mut child_sum = 0;
+                if node.borrow().left.is_some() {
+                    child_sum += node.borrow().left.as_ref().unwrap().borrow().val;
+                }
+                if node.borrow().right.is_some() {
+                    child_sum += node.borrow().right.as_ref().unwrap().borrow().val;
+                }
+                if node.borrow().left.is_some() {
+                    new_queue
+                        .push_back((Rc::clone(node.borrow().left.as_ref().unwrap()), child_sum));
+                }
+                if node.borrow().right.is_some() {
+                    new_queue
+                        .push_back((Rc::clone(node.borrow().right.as_ref().unwrap()), child_sum));
+                }
+            }
+            queue = new_queue;
+        }
+        root
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        let tree_val = [3, 5, 1, 6, 2, 9, 8, i32::MAX, i32::MAX, 7, 4];
+        let mut root = TreeNode::create_from_vec(tree_val.to_vec());
+        root = Solution::replace_value_in_tree(root);
+        let result = TreeNode::to_vec(root);
+        assert_eq!(result, vec![1, 2, 5], "The results are not as expected.");
     }
 }
