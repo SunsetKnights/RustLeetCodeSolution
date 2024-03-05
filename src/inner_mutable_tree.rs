5b13e62b18de1b885f7cb993681177defe98085a
SunsetKnights <sunsetknightss@gmail.com>
2024-03-05 14:09:19 +0800

    1976. 到达目的地的方案数

--- src/inner_mutable_tree.rs
+++ src/inner_mutable_tree.rs
@@ -594,6 +594,50 @@ impl Solution {
         }
         ret
     }
+
+    /**
+     * 1976. 到达目的地的方案数
+     * 你在一个城市里，城市由 n 个路口组成，路口编号为 0 到 n - 1 ，
+     * 某些路口之间有 双向 道路。输入保证你可以从任意路口出发到达其他任意路口，且任意两个路口之间最多有一条路。
+     * 给你一个整数 n 和二维整数数组 roads ，其中 roads[i] = [ui, vi, timei] 表示在路口 ui 和 vi 之间有一条需要花费 timei 时间才能通过的道路。
+     * 你想知道花费 最少时间 从路口 0 出发到达路口 n - 1 的方案数。
+     * 请返回花费 最少时间 到达目的地的 路径数目 。由于答案可能很大，将结果对 109 + 7 取余 后返回。
+     */
+    pub fn count_paths(n: i32, roads: Vec<Vec<i32>>) -> i32 {
+        use std::cmp::Reverse;
+        use std::collections::BinaryHeap;
+        let mut adj = vec![vec![]; n as usize];
+        for r in roads {
+            adj[r[0] as usize].push((r[1], r[2]));
+            adj[r[1] as usize].push((r[0], r[2]));
+        }
+        let mut distance = vec![i64::MAX; n as usize];
+        let mut visited = vec![false; n as usize];
+        let mut min = BinaryHeap::new();
+        let mut ways = vec![0; n as usize];
+        ways[0] = 1;
+        distance[0] = 0;
+        min.push((Reverse(0), 0));
+        while let Some((_, curr)) = min.pop() {
+            let curr = curr as usize;
+            if visited[curr] {
+                continue;
+            }
+            visited[curr] = true;
+            for &adj_nodes in &adj[curr] {
+                let node = adj_nodes.0 as usize;
+                let dis = adj_nodes.1 as i64;
+                if distance[curr] + dis < distance[node] {
+                    distance[node] = distance[curr] + dis;
+                    min.push((Reverse(distance[node]), adj_nodes.0));
+                    ways[node] = ways[curr];
+                } else if distance[curr] + dis == distance[node] {
+                    ways[node] = (ways[curr] + ways[node]) % 1_000_000_007;
+                }
+            }
+        }
+        ways[n as usize - 1]
+    }
 }
 
 #[cfg(test)]
