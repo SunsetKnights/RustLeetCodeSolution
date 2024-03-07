9e37beba7d3ebcd03a4f2a31c512f17445707c5c
SunsetKnights <sunsetknightss@gmail.com>
2024-03-07 10:41:28 +0800

    2575. 找出字符串的可整除数组

--- src/inner_mutable_tree.rs
+++ src/inner_mutable_tree.rs
@@ -638,6 +638,27 @@ impl Solution {
         }
         ways[n as usize - 1]
     }
+
+    /**
+     * 2575. 找出字符串的可整除数组
+     * 给你一个下标从 0 开始的字符串 word ，长度为 n ，由从 0 到 9 的数字组成。另给你一个正整数 m 。
+     * word 的 可整除数组 div  是一个长度为 n 的整数数组，并满足：
+     *     如果 word[0,...,i] 所表示的 数值 能被 m 整除，div[i] = 1
+     *     否则，div[i] = 0
+     * 返回 word 的可整除数组。
+     */
+    pub fn divisibility_array(word: String, m: i32) -> Vec<i32> {
+        let mut remainder = 0;
+        word.chars()
+            .map(|n| {
+                remainder = (remainder * 10 + (n.to_digit(10).unwrap() as i64)) % m as i64;
+                match remainder {
+                    0 => 1,
+                    _ => 0,
+                }
+            })
+            .collect()
+    }
 }
 
 #[cfg(test)]
