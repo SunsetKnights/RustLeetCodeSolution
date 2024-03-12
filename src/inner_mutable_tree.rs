81169066736ec7475bcd36137763d2214a9b6229
SunsetKnights <sunsetknightss@gmail.com>
2024-03-12 12:00:05 +0800

    1261. 在受污染的二叉树中查找元素

--- src/inner_mutable_tree.rs
+++ src/inner_mutable_tree.rs
@@ -596,6 +596,46 @@ impl Solution {
     }
 }
 
+/**
+ * 1261. 在受污染的二叉树中查找元素
+ * 给出一个满足下述规则的二叉树：
+ *     root.val == 0
+ *     如果 treeNode.val == x 且 treeNode.left != null，那么 treeNode.left.val == 2 * x + 1
+ *     如果 treeNode.val == x 且 treeNode.right != null，那么 treeNode.right.val == 2 * x + 2
+ * 现在这个二叉树受到「污染」，所有的 treeNode.val 都变成了 -1。
+ * 请你先还原二叉树，然后实现 FindElements 类：
+ *     FindElements(TreeNode* root) 用受污染的二叉树初始化对象，你需要先把它还原。
+ *     bool find(int target) 判断目标值 target 是否存在于还原后的二叉树中并返回结果。
+ */
+use std::collections::HashSet;
+struct FindElements {
+    hash: HashSet<i32>,
+}
+impl FindElements {
+    fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
+        let mut ret = Self {
+            hash: HashSet::new(),
+        };
+        fn dfs(val: i32, hash: &mut HashSet<i32>, node: Rc<RefCell<TreeNode>>) {
+            hash.insert(val);
+            if let Some(left) = node.borrow_mut().left.take() {
+                dfs(val * 2 + 1, hash, left);
+            };
+            if let Some(right) = node.borrow_mut().right.take() {
+                dfs(val * 2 + 2, hash, right);
+            };
+        }
+        if let Some(node) = root {
+            dfs(0, &mut ret.hash, node);
+        }
+        ret
+    }
+
+    fn find(&self, target: i32) -> bool {
+        self.hash.contains(&target)
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
