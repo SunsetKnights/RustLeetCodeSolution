2ad3f0b2201077744595a6cb454cb460a7742079
SunsetKnights <sunsetknightss@gmail.com>
2024-03-14 11:12:37 +0800

    2789. 合并后数组中的最大元素

--- src/functions_2.rs
+++ src/functions_2.rs
@@ -677,4 +677,23 @@ impl Solution {
         }
         format!("{a}A{}B", b - a)
     }
+
+    /**
+     * 2789. 合并后数组中的最大元素
+     * 给你一个下标从 0 开始、由正整数组成的数组 nums 。
+     * 你可以在数组上执行下述操作 任意 次：
+     *     选中一个同时满足 0 <= i < nums.length - 1 和 nums[i] <= nums[i + 1] 的整数 i 。
+     *     将元素 nums[i + 1] 替换为 nums[i] + nums[i + 1] ，并从数组中删除元素 nums[i] 。
+     * 返回你可以从最终数组中获得的 最大 元素的值。
+     */
+    pub fn max_array_value(mut nums: Vec<i32>) -> i64 {
+        let mut ret = 0;
+        for n in nums.iter().rev() {
+            ret = match ret >= *n as i64 {
+                true => ret + *n as i64,
+                false => *n as i64,
+            }
+        }
+        ret
+    }
 }
