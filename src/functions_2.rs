636f02f27cc9645e9abf3418677f7cf6155a0460
SunsetKnights <sunsetknightss@gmail.com>
2024-03-15 10:55:09 +0800

    2312. 卖木头块

--- src/functions_2.rs
+++ src/functions_2.rs
@@ -696,4 +696,33 @@ impl Solution {
         }
         ret
     }
+
+    /**
+     * 2312. 卖木头块
+     * 给你两个整数 m 和 n ，分别表示一块矩形木块的高和宽。
+     * 同时给你一个二维整数数组 prices ，其中 prices[i] = [hi, wi, pricei] 表示你可以以 pricei 元的价格卖一块高为 hi 宽为 wi 的矩形木块。
+     * 每一次操作中，你必须按下述方式之一执行切割操作，以得到两块更小的矩形木块：
+     *     沿垂直方向按高度 完全 切割木块，或
+     *     沿水平方向按宽度 完全 切割木块
+     * 在将一块木块切成若干小木块后，你可以根据 prices 卖木块。你可以卖多块同样尺寸的木块。你不需要将所有小木块都卖出去。你 不能 旋转切好后木块的高和宽。
+     * 请你返回切割一块大小为 m x n 的木块后，能得到的 最多 钱数。
+     * 注意你可以切割木块任意次。
+     */
+    pub fn selling_wood(m: i32, n: i32, mut prices: Vec<Vec<i32>>) -> i64 {
+        let mut p = vec![vec![0; (n + 1) as usize]; (m + 1) as usize];
+        for price in prices {
+            p[price[0] as usize][price[1] as usize] = price[2] as i64;
+        }
+        for i in 1..=m as usize {
+            for j in 1..=n as usize {
+                for k in 1..j {
+                    p[i][j] = p[i][j].max(p[i][k] + p[i][j - k]);
+                }
+                for k in 1..i {
+                    p[i][j] = p[i][j].max(p[k][j] + p[i - k][j]);
+                }
+            }
+        }
+        p[m as usize][n as usize]
+    }
 }
