63e2cd85c94290124b00c8ecf5b171f34e44d6d3
SunsetKnights <sunsetknightss@gmail.com>
2024-03-17 15:27:11 +0800

    310. 最小高度树

--- src/functions_2.rs
+++ src/functions_2.rs
@@ -760,4 +760,64 @@ impl Solution {
         }
         ret
     }
+
+    /**
+     * 310. 最小高度树
+     * 树是一个无向图，其中任何两个顶点只通过一条路径连接。 换句话说，一个任何没有简单环路的连通图都是一棵树。
+     * 给你一棵包含 n 个节点的树，标记为 0 到 n - 1 。
+     * 给定数字 n 和一个有 n - 1 条无向边的 edges 列表（每一个边都是一对标签），
+     * 其中 edges[i] = [ai, bi] 表示树中节点 ai 和 bi 之间存在一条无向边。
+     * 可选择树中任何一个节点作为根。当选择节点 x 作为根节点时，设结果树的高度为 h 。
+     * 在所有可能的树中，具有最小高度的树（即，min(h)）被称为 最小高度树 。
+     * 请你找到所有的 最小高度树 并按 任意顺序 返回它们的根节点标签列表。
+     * 树的 高度 是指根节点和叶子节点之间最长向下路径上边的数量。
+     */
+    pub fn find_min_height_trees(n: i32, mut edges: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut g = vec![vec![]; n as usize];
+        for e in edges {
+            g[e[0] as usize].push(e[1]);
+            g[e[1] as usize].push(e[0]);
+        }
+        fn dfs(g: &Vec<Vec<i32>>, node: i32, from: i32) -> (i32, i32) {
+            let mut max_distance = 0;
+            let mut max_node = node;
+            for &next in &g[node as usize] {
+                if next != from {
+                    let (m, n) = dfs(g, next, node);
+                    if m + 1 > max_distance {
+                        max_distance = m + 1;
+                        max_node = n;
+                    }
+                }
+            }
+            (max_distance, max_node)
+        }
+        let x = dfs(&g, 0, 0).1;
+        let y = dfs(&g, x, x).1;
+        let mut way = vec![];
+        fn find_way(
+            g: &Vec<Vec<i32>>,
+            node: i32,
+            from: i32,
+            target: i32,
+            way: &mut Vec<i32>,
+        ) -> bool {
+            if node == target {
+                way.push(node);
+                return true;
+            }
+            for &next in &g[node as usize] {
+                if next != from && find_way(g, next, node, target, way) {
+                    way.push(node);
+                    return true;
+                }
+            }
+            false
+        }
+        find_way(&g, x, x, y, &mut way);
+        match way.len() % 2 == 0 {
+            true => vec![way[way.len() / 2 - 1], way[way.len() / 2]],
+            false => vec![way[way.len() / 2]],
+        }
+    }
 }
