8c884d4cf9dffdd09bb326b912d2f663655a88c4
SunsetKnights <sunsetknightss@gmail.com>
2024-03-25 09:24:41 +0800

    518. 零钱兑换 II

--- src/functions_2.rs
+++ src/functions_2.rs
@@ -905,4 +905,22 @@ impl Solution {
             res => res,
         }
     }
+
+    /**
+     * 518. 零钱兑换 II
+     * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+     * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+     * 假设每一种面额的硬币有无限个。
+     * 题目数据保证结果符合 32 位带符号整数。
+     */
+    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+        let mut memory = vec![0; (amount + 1) as usize];
+        memory[0] = 1;
+        for c in coins {
+            for i in c..=amount {
+                memory[i as usize] += memory[(i - c) as usize];
+            }
+        }
+        memory[amount as usize]
+    }
 }
