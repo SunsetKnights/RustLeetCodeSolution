c36b2c5a7c462179fa88bf7cdf13b2284b8f0203
SunsetKnights <sunsetknightss@gmail.com>
2024-03-28 10:38:31 +0800

    1997. 访问完所有房间的第一天

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -27,4 +27,32 @@ impl Solution {
         }
         res as i32
     }
+
+    /**
+     * 1997. 访问完所有房间的第一天
+     * 你需要访问 n 个房间，房间从 0 到 n - 1 编号。同时，每一天都有一个日期编号，从 0 开始，依天数递增。你每天都会访问一个房间。
+     * 最开始的第 0 天，你访问 0 号房间。给你一个长度为 n 且 下标从 0 开始 的数组 nextVisit 。在接下来的几天中，你访问房间的 次序 将根据下面的 规则 决定：
+     *     假设某一天，你访问 i 号房间。
+     *     如果算上本次访问，访问 i 号房间的次数为 奇数 ，那么 第二天 需要访问 nextVisit[i] 所指定的房间，其中 0 <= nextVisit[i] <= i 。
+     *     如果算上本次访问，访问 i 号房间的次数为 偶数 ，那么 第二天 需要访问 (i + 1) mod n 号房间。
+     * 请返回你访问完所有房间的第一天的日期编号。题目数据保证总是存在这样的一天。由于答案可能很大，返回对 109 + 7 取余后的结果。
+     *
+     * n == nextVisit.length
+     * 2 <= n <= 105
+     * 0 <= nextVisit[i] <= i
+     */
+    pub fn first_day_been_in_all_rooms(next_visit: Vec<i32>) -> i32 {
+        let mut prefix = vec![0; next_visit.len()];
+        // 第一次到达0+1号房间的天数是2
+        prefix[0] = 2;
+        for i in 1..next_visit.len() {
+            prefix[i] = prefix[i - 1] + 2;
+            if next_visit[i] != 0 {
+                prefix[i] = (prefix[i] - prefix[(next_visit[i] - 1) as usize] + 1_000_000_007)
+                    % 1_000_000_007;
+            }
+            prefix[i] = (prefix[i] + prefix[i - 1]) % 1_000_000_007;
+        }
+        prefix[next_visit.len() - 2]
+    }
 }
