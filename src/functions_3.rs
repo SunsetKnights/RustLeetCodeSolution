116102383638ccbe99f227b11bdc795307c334fb
SunsetKnights <sunsetknightss@gmail.com>
2024-03-30 10:36:19 +0800

    2952. 需要添加的硬币的最小数量

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -55,4 +55,27 @@ impl Solution {
         }
         prefix[next_visit.len() - 2]
     }
+
+    /**
+     * 2952. 需要添加的硬币的最小数量
+     * 给你一个下标从 0 开始的整数数组 coins，表示可用的硬币的面值，以及一个整数 target 。
+     * 如果存在某个 coins 的子序列总和为 x，那么整数 x 就是一个 可取得的金额 。
+     * 返回需要添加到数组中的 任意面值 硬币的 最小数量 ，使范围 [1, target] 内的每个整数都属于 可取得的金额 。
+     * 数组的 子序列 是通过删除原始数组的一些（可能不删除）元素而形成的新的 非空 数组，删除过程不会改变剩余元素的相对位置。
+     */
+    pub fn minimum_added_coins(mut coins: Vec<i32>, target: i32) -> i32 {
+        coins.sort_unstable();
+        let (mut added, mut curr_complete, mut i) = (0, 1, 0usize);
+        while curr_complete <= target {
+            if i < coins.len() && coins[i] <= curr_complete {
+                curr_complete += coins[i];
+                i += 1;
+            } else {
+                // add num curr_complete
+                curr_complete += curr_complete;
+                added += 1;
+            }
+        }
+        added
+    }
 }
