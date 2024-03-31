db3e49b87231faee9ab9fc8daa4dc51a5bcbc378
SunsetKnights <sunsetknightss@gmail.com>
2024-03-31 13:29:03 +0800

    331. 验证二叉树的前序序列化

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -78,4 +78,40 @@ impl Solution {
         }
         added
     }
+
+    /**
+     * 331. 验证二叉树的前序序列化
+     * 序列化二叉树的一种方法是使用 前序遍历 。当我们遇到一个非空节点时，我们可以记录下这个节点的值。如果它是一个空节点，我们可以使用一个标记值记录，例如 #。
+     * 给定一串以逗号分隔的序列，验证它是否是正确的二叉树的前序序列化。编写一个在不重构树的条件下的可行算法。
+     * 保证 每个以逗号分隔的字符或为一个整数或为一个表示 null 指针的 '#' 。
+     * 你可以认为输入格式总是有效的
+     *     例如它永远不会包含两个连续的逗号，比如 "1,,3" 。
+     * 注意：不允许重建树。
+     */
+    pub fn is_valid_serialization(preorder: String) -> bool {
+        let chars: Vec<char> = preorder
+            .split(',')
+            .map(|s| s.chars().next().unwrap())
+            .collect();
+        let mut stack = Vec::new();
+        for c in chars {
+            stack.push(c);
+            let mut len = stack.len();
+            let mut top = c;
+            while top == '#'
+                && len >= 3
+                && stack[len - 1] == '#'
+                && stack[len - 2] == '#'
+                && stack[len - 3] != '#'
+            {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.push('#');
+                top = '#';
+                len = stack.len();
+            }
+        }
+        stack.len() == 1 && stack[0] == '#'
+    }
 }
