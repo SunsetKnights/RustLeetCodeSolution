725f1c323a4e3b08accc06854f435f1cd33921a2
SunsetKnights <sunsetknightss@gmail.com>
2024-04-10 10:57:13 +0800

    1702. 修改后的最大二进制字符串

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -146,4 +146,21 @@ impl Solution {
         }
         res
     }
+    /**
+     * 1702. 修改后的最大二进制字符串
+     * 给你一个二进制字符串 binary ，它仅有 0 或者 1 组成。你可以使用下面的操作任意次对它进行修改：
+     *     操作 1 ：如果二进制串包含子字符串 "00" ，你可以用 "10" 将其替换。
+     *         比方说， "00010" -> "10010"
+     *     操作 2 ：如果二进制串包含子字符串 "10" ，你可以用 "01" 将其替换。
+     *         比方说， "00010" -> "00001"
+     * 请你返回执行上述操作任意次以后能得到的 最大二进制字符串 。
+     * 如果二进制字符串 x 对应的十进制数字大于二进制字符串 y 对应的十进制数字，那么我们称二进制字符串 x 大于二进制字符串 y 。
+     */
+    pub fn maximum_binary_string(binary: String) -> String {
+        if let Some(i) = binary.find('0') {
+            let one_count = binary[i..].chars().filter(|&c| c == '1').count();
+            return "1".repeat(binary.len() - 1 - one_count) + "0" + &"1".repeat(one_count);
+        }
+        binary
+    }
 }
