c0409844d49df6890b7ac5252ec9d6f6e92683bf
SunsetKnights <sunsetknightss@gmail.com>
2024-04-20 11:06:57 +0800

    39. 组合总和

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -665,4 +665,36 @@ impl Solution {
         }
         -1
     }
+    /**
+     * 39. 组合总和
+     * 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，
+     * 找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+     * candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+     * 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+     */
+    pub fn combination_sum(mut candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        candidates.sort_unstable();
+        let mut res = vec![];
+        fn dfs(
+            i: usize,
+            left: i32,
+            candidates: &Vec<i32>,
+            comb: &mut Vec<i32>,
+            res: &mut Vec<Vec<i32>>,
+        ) {
+            if left == 0 {
+                res.push(comb.clone());
+                return;
+            }
+            if i == candidates.len() || left < 0 || left < candidates[i] {
+                return;
+            }
+            dfs(i + 1, left, candidates, comb, res);
+            comb.push(candidates[i]);
+            dfs(i, left - candidates[i], candidates, comb, res);
+            comb.pop();
+        }
+        dfs(0, target, &candidates, &mut vec![], &mut res);
+        res
+    }
 }
