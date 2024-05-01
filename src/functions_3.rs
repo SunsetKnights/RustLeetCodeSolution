588a4cd1d4968ba4a7f679b80980978b14174cc8
SunsetKnights <sunsetknightss@gmail.com>
2024-05-01 11:11:11 +0800

    2462. 雇佣 K 位工人的总代价

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -697,4 +697,60 @@ impl Solution {
         dfs(0, target, &candidates, &mut vec![], &mut res);
         res
     }
+    /**
+     * 2462. 雇佣 K 位工人的总代价
+     * 给你一个下标从 0 开始的整数数组 costs ，其中 costs[i] 是雇佣第 i 位工人的代价。
+     * 同时给你两个整数 k 和 candidates 。我们想根据以下规则恰好雇佣 k 位工人：
+     *     总共进行 k 轮雇佣，且每一轮恰好雇佣一位工人。
+     *     在每一轮雇佣中，从最前面 candidates 和最后面 candidates 人中选出代价最小的一位工人，如果有多位代价相同且最小的工人，选择下标更小的一位工人。
+     *         比方说，costs = [3,2,7,7,1,2] 且 candidates = 2 ，第一轮雇佣中，我们选择第 4 位工人，因为他的代价最小 [3,2,7,7,1,2] 。
+     *         第二轮雇佣，我们选择第 1 位工人，因为他们的代价与第 4 位工人一样都是最小代价，而且下标更小，[3,2,7,7,2] 。注意每一轮雇佣后，剩余工人的下标可能会发生变化。
+     *     如果剩余员工数目不足 candidates 人，那么下一轮雇佣他们中代价最小的一人，如果有多位代价相同且最小的工人，选择下标更小的一位工人。
+     *     一位工人只能被选择一次。
+     * 返回雇佣恰好 k 位工人的总代价。
+     */
+    pub fn total_cost(mut costs: Vec<i32>, k: i32, candidates: i32) -> i64 {
+        use std::cmp::Reverse;
+        use std::collections::BinaryHeap;
+        let n = costs.len();
+        if k as usize >= n {
+            return costs.iter().map(|&c| c as i64).sum();
+        }
+        if candidates as usize * 2 >= n {
+            costs.sort_unstable();
+            return costs.iter().take(k as usize).map(|&c| c as i64).sum();
+        }
+        let mut prefix =
+            BinaryHeap::from_iter(costs.iter().take(candidates as usize).map(|&c| Reverse(c)));
+        let mut suffix = BinaryHeap::from_iter(
+            costs
+                .iter()
+                .rev()
+                .take(candidates as usize)
+                .map(|&c| Reverse(c)),
+        );
+        let mut pre_end = candidates - 1;
+        let mut suf_start = n as i32 - candidates - 1;
+        let mut res = 0;
+        for _ in 0..k {
+            if prefix.is_empty() {
+                res += suffix.pop().unwrap().0 as i64;
+            } else if suffix.is_empty() {
+                res += prefix.pop().unwrap().0 as i64;
+            } else if prefix.peek().unwrap().0 <= suffix.peek().unwrap().0 {
+                res += prefix.pop().unwrap().0 as i64;
+                if pre_end < suf_start {
+                    pre_end += 1;
+                    prefix.push(Reverse(costs[pre_end as usize]));
+                }
+            } else {
+                res += suffix.pop().unwrap().0 as i64;
+                if pre_end < suf_start {
+                    suffix.push(Reverse(costs[suf_start as usize]));
+                    suf_start -= 1;
+                }
+            }
+        }
+        res
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,5 @@ mod functions_3;
 mod inner_mutable_tree;
 
 fn main() {
-    functions_3::Solution::min_malware_spread_ii(
-        vec![
-            vec![1, 0, 0, 0, 0, 0, 0, 0, 1],
-            vec![0, 1, 0, 1, 0, 0, 0, 0, 0],
-            vec![0, 0, 1, 1, 0, 1, 0, 0, 0],
-            vec![0, 1, 1, 1, 1, 0, 1, 0, 0],
-            vec![0, 0, 0, 1, 1, 1, 0, 0, 0],
-            vec![0, 0, 1, 0, 1, 1, 0, 0, 0],
-            vec![0, 0, 0, 1, 0, 0, 1, 1, 0],
-            vec![0, 0, 0, 0, 0, 0, 1, 1, 1],
-            vec![1, 0, 0, 0, 0, 0, 0, 1, 1],
-        ],
-        vec![3, 7],
-    );
+    functions_3::Solution::total_cost(vec![17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4);
 }
