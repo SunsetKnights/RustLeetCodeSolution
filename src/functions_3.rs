a314b0121c832c816061870091fea9431e20396d
SunsetKnights <sunsetknightss@gmail.com>
2024-05-04 10:02:03 +0800

    1235. 规划兼职工作

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -785,4 +785,29 @@ impl Solution {
         }
         res
     }
+    /**
+     * 1235. 规划兼职工作
+     * 你打算利用空闲时间来做兼职工作赚些零花钱。
+     * 这里有 n 份兼职工作，每份工作预计从 startTime[i] 开始到 endTime[i] 结束，报酬为 profit[i]。
+     * 给你一份兼职工作表，包含开始时间 startTime，结束时间 endTime 和预计报酬 profit 三个数组，请你计算并返回可以获得的最大报酬。
+     * 注意，时间上出现重叠的 2 份工作不能同时进行。
+     * 如果你选择的工作在时间 X 结束，那么你可以立刻进行在时间 X 开始的下一份工作。
+     */
+    pub fn job_scheduling(start_time: Vec<i32>, end_time: Vec<i32>, profit: Vec<i32>) -> i32 {
+        let n = start_time.len();
+        let mut info = start_time
+            .iter()
+            .zip(end_time.iter())
+            .zip(profit.iter())
+            .map(|((&a, &b), &c)| (a, b, c))
+            .collect::<Vec<_>>();
+        info.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+        let mut memory = vec![0; n + 1];
+        for i in 0..n {
+            let curr_start = info[i].0;
+            let pre = info.partition_point(|x| x.1 <= curr_start);
+            memory[i + 1] = memory[i].max(info[i].2 + memory[pre]);
+        }
+        memory[n]
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ mod functions_3;
 mod inner_mutable_tree;
 
 fn main() {
-    functions_3::Solution::total_cost(vec![17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4);
+    functions_3::Solution::job_scheduling(
+        vec![1, 2, 3, 4, 6],
+        vec![3, 5, 10, 6, 9],
+        vec![20, 20, 100, 70, 60],
+    );
 }
