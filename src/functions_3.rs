4ebc755426e2cc9aaf08e715faa7e223cea5cec6
SunsetKnights <sunsetknightss@gmail.com>
2024-05-07 23:19:23 +0800

    1463. 摘樱桃 II

--- src/functions_3.rs
+++ src/functions_3.rs
@@ -810,4 +810,49 @@ impl Solution {
         }
         memory[n]
     }
+    /**
+     * 1463. 摘樱桃 II
+     * 给你一个 rows x cols 的矩阵 grid 来表示一块樱桃地。 grid 中每个格子的数字表示你能获得的樱桃数目。
+     * 你有两个机器人帮你收集樱桃，机器人 1 从左上角格子 (0,0) 出发，机器人 2 从右上角格子 (0, cols-1) 出发。
+     * 请你按照如下规则，返回两个机器人能收集的最多樱桃数目：
+     *     从格子 (i,j) 出发，机器人可以移动到格子 (i+1, j-1)，(i+1, j) 或者 (i+1, j+1) 。
+     *     当一个机器人经过某个格子时，它会把该格子内所有的樱桃都摘走，然后这个位置会变成空格子，即没有樱桃的格子。
+     *     当两个机器人同时到达同一个格子时，它们中只有一个可以摘到樱桃。
+     *     两个机器人在任意时刻都不能移动到 grid 外面。
+     *     两个机器人最后都要到达 grid 最底下一行。
+     */
+    pub fn cherry_pickup(grid: Vec<Vec<i32>>) -> i32 {
+        let row = grid.len();
+        let col = grid[0].len();
+        let mut front = vec![vec![0; col + 2]; col + 2];
+        let mut current = vec![vec![0; col + 2]; col + 2];
+        // step 0, robot #1 in column 0, robot #2 in column col-1
+        let mut res = 0;
+        front[1][col] = grid[0][0] + grid[0][col - 1];
+        for step in 1..row {
+            for i in 1..(col + 1).min(step + 2) {
+                for j in col.saturating_sub(step).max(i + 1)..=col {
+                    current[i][j] = *[
+                        front[i - 1][j - 1],
+                        front[i][j - 1],
+                        front[i + 1][j - 1],
+                        front[i - 1][j],
+                        front[i][j],
+                        front[i + 1][j],
+                        front[i - 1][j + 1],
+                        front[i][j + 1],
+                        front[i + 1][j + 1],
+                    ]
+                    .iter()
+                    .max()
+                    .unwrap()
+                        + grid[step][i - 1]
+                        + grid[step][j - 1];
+                    res = res.max(current[i][j]);
+                }
+            }
+            std::mem::swap(&mut current, &mut front);
+        }
+        res
+    }
 }
