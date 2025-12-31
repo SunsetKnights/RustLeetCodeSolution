948089bddbc70d81dcda574e953a57a46724d554
SunsetKnights <sunsetknightss@gmail.com>
2025-12-31 16:18:26 +0800

    some solutions

--- src/functions_4.rs
+++ src/functions_4.rs
@@ -57,4 +57,104 @@ impl Solution {
         }
         result
     }
+    /**
+     * 2845. 统计趣味子数组的数目
+     * 给你一个下标从 0 开始的整数数组 nums ，以及整数 modulo 和整数 k 。
+     * 请你找出并统计数组中 趣味子数组 的数目。
+     * 如果 子数组 nums[l..r] 满足下述条件，则称其为 趣味子数组 ：
+     *     在范围 [l, r] 内，设 cnt 为满足 nums[i] % modulo == k 的索引 i 的数量。并且 cnt % modulo == k 。
+     * 以整数形式表示并返回趣味子数组的数目。
+     * 注意：子数组是数组中的一个连续非空的元素序列。
+     */
+    pub fn count_interesting_subarrays(nums: Vec<i32>, modulo: i32, k: i32) -> i64 {
+        let mut count = 0;
+        let mut result = 0;
+        let mut map = std::collections::HashMap::new();
+        map.insert(0, 1);
+        for i in nums {
+            if i % modulo == k {
+                count += 1;
+            }
+            let key = (count - k) % modulo;
+            if let Some(&value) = map.get(&key) {
+                result += value;
+            }
+            *map.entry(count % modulo).or_insert(0) += 1;
+        }
+        result
+    }
+    /**
+     * 2444. 统计定界子数组的数目
+     * 给你一个整数数组 nums 和两个整数 minK 以及 maxK 。
+     * nums 的定界子数组是满足下述条件的一个子数组：
+     *     子数组中的 最小值 等于 minK 。
+     *     子数组中的 最大值 等于 maxK 。
+     * 返回定界子数组的数目。
+     * 子数组是数组中的一个连续部分。
+     */
+    pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
+        let n = nums.len();
+        let mut result = 0;
+        let mut min_idx = None;
+        let mut max_idx = None;
+        let mut start = 0;
+        for i in 0..n {
+            // 过界，更新下一段的起始点
+            if nums[i] < min_k || nums[i] > max_k {
+                start = i + 1;
+                min_idx = None;
+                max_idx = None;
+            } else {
+                if nums[i] == min_k {
+                    min_idx = Some(i);
+                }
+                if nums[i] == max_k {
+                    max_idx = Some(i);
+                }
+                // 最大最小值已找到且没有过界，枚举结束点
+                if let (Some(min), Some(max)) = (min_idx.clone(), max_idx.clone()) {
+                    result += (min.min(max) - start + 1) as i64;
+                }
+            }
+        }
+        result
+    }
+    /**
+     * 2962. 统计最大元素出现至少 K 次的子数组
+     * 给你一个整数数组 nums 和一个 正整数 k 。
+     * 请你统计有多少满足 「 nums 中的 最大 元素」至少出现 k 次的子数组，并返回满足这一条件的子数组的数目。
+     * 子数组是数组中的一个连续元素序列。
+     */
+    pub fn count_subarrays_2962(nums: Vec<i32>, k: i32) -> i64 {
+        let n = nums.len();
+        let mut locations = Vec::new();
+        let mut result = 0;
+        let mut max_num = 0;
+        nums.iter().enumerate().for_each(|(i, &n)| {
+            if n > max_num {
+                max_num = n;
+                locations.clear();
+                locations.push(i);
+            }
+            if n == max_num {
+                locations.push(i);
+            }
+        });
+        if locations.len() < k as usize {
+            return 0;
+        }
+        // 枚举起始点
+        let mut left = 0;
+        while left + k as usize - 1 < locations.len() {
+            let right = locations[left + k as usize - 1];
+            let left_bound = if left == 0 {
+                locations[left] + 1
+            } else {
+                locations[left] - locations[left - 1]
+            };
+            result += left_bound as i64 * (n - right) as i64;
+            left += 1;
+        }
+        result
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod functions_4;
 mod inner_mutable_tree;
-mod lru_cache;
 
 fn main() {
     inner_mutable_tree::Solution::max_path_sum(inner_mutable_tree::TreeNode::from_vec(vec![
